215d8235a6cfc47dd54ae7ab788e238816039cb1
Benjamin McLemore <mclemorebenjamin@gmail.com>
2021-02-15 23:14:25 -0500

    Delete fns.py and .DS_Store

--- ttable.py
+++ ttable.py
@@ -1,6 +1,93 @@
 #!/usr/bin/python3
 import sys
-from fns import *
+
+# global variables
+digits = {'0','1'}
+ops = ('+','*')
+
+# adds two values
+def add(a,b):
+    if a == '1' or b == '1':
+        return '1'
+    else:
+        return '0'
+
+# multplies two values
+def mult(a,b):
+    if a=='1' and b=='1':
+        return '1'
+    else: 
+        return '0'
+
+# negates a value
+def neg(a):
+    if a=='1':
+        return '0'
+    else:
+        return '1'
+
+# returns a list of maps given the number of variables
+def var_maps(vars):
+    maps = []
+    for i in range(2**len(vars)):
+        map = {}
+        for var in sorted(vars,reverse=True):
+            map[var] = str(i%2)
+            i = i//2
+        maps.append(map)
+    return maps
+
+# calculates based on a given operator
+def calc(a,b,op):
+    if op =='+':
+        val = add(a,b)
+        return val
+    elif op=='*':
+        val = mult(a,b)
+        return val
+    else:
+        raise Exception("Invalid Operator")
+
+# evaluates the expression
+def eval(expr):
+    
+    # init stacks
+    val_stack = []
+    op_stack = []
+
+    # loop over the expression
+    for c in expr:
+        if c in digits:
+            val_stack.append(c)
+        elif c == '\'':
+            val_stack.append(neg(val_stack.pop()))
+        elif c in ops and (len(op_stack)<1 or op_stack[-1]=='('):
+            op_stack.append(c)
+        elif c in ops:
+            while len(op_stack)>0:
+                prev_op = op_stack.pop()
+                if prev_op in ops and ops.index(c) < ops.index(prev_op):
+                    b = val_stack.pop()
+                    a = val_stack.pop()
+                    val_stack.append(calc(a,b,prev_op))
+                else:
+                    break
+            op_stack.append(c)
+        elif c =='(':
+            op_stack.append(c)
+        elif c ==')':
+            while len(op_stack)>0:
+                b = val_stack.pop()
+                a = val_stack.pop()
+                op = op_stack.pop()
+                while op == '(':
+                    op = op_stack.pop()
+                val_stack.append(calc(a,b,op))
+        else:
+            raise Exception("Unrecognized Token: "+c)
+    
+    # return statement
+    return val_stack[0]
 
 # get variables
 equation = sys.argv[1].replace(' ','').replace("\\","")
@@ -29,6 +116,3 @@ for map in var_maps(vars):
     
     # substitute every variable in the equ
     print(eval(expr))
-
-
-print(expression)
